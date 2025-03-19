d86813c09f3b233d512d4567f03e6b9fb84dd23e
DanielBerith <danielberith@gmail.com>
2025-03-19 18:51:53 +0200

    Implement Domain Layer: Define Elevator and Request entities with associated enums for Direction and ElevatorType
    - Create the Elevator entity with properties: Id, CurrentFloor, Direction, Occupants, MaxCapacity, and ElevatorType.
    - Implement controlled access to Elevator state with private setters and getter methods.
    - Define Direction and ElevatorType enums to represent the possible states of the elevator and its type.
    - Create a Request entity to represent elevator requests, including the floor and the number of passengers.

---
+++ ElevatorSimulation.Domain/Entities/Elevator.cs
@@ -0,0 +1,34 @@
+﻿using ElevatorSimulation.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevatorSimulation.Domain.Entities
+{
+    public class Elevator
+    {
+        public int Id { get; set; }
+        public int CurrentFloor { get; private set; }
+        public Direction Direction { get; private set; }
+        public int Occupants { get; private set; }
+        public int MaxCapacity { get; private set; }
+        public ElevatorType Type { get; private set; }
+
+        public Elevator(int id, int initialFloor, int maxCapacity, ElevatorType type)
+        {
+            Id = id;
+            CurrentFloor = initialFloor;
+            MaxCapacity = maxCapacity;
+            Type = type;
+            Direction = Direction.Idle;
+        }
+
+        // Getters for state
+        public int GetCurrentFloor() => CurrentFloor;
+        public Direction GetDirection() => Direction;
+        public int GetOccupants() => Occupants;
+        public int GetMaxCapacity() => MaxCapacity;
+    }
+}
---
+++ ElevatorSimulation.Domain/Enums/ElevatorType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevatorSimulation.Domain.Enums
+{
+    public enum ElevatorType
+    {
+        Passenger,
+        Freight,
+        HighSpeed
+    }
+}
